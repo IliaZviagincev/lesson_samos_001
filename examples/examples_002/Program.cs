a684db2dee4b288dd1bd7b1e7bfdd2dd4b86203c
Ilia_Z <akinoro19@gmail.com>
2022-07-08 13:27:58 +0300

    example_002 is summ calc

---
+++ examples/examples_002/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число  ");
+int a = Int32.Parse(Console.ReadLine());
+Console.Write("Введите второе число   ");
+int b = Int32.Parse(Console.ReadLine());
+int summ = a + b;
+Console.Write("Сумма этих чисел равна   ");
+Console.WriteLine(summ);
